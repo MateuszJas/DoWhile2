0e085a20cbb76d68d722de50948d4b9ae200ac99
MateuszJas <pphu.jaskolka@hotmail.com>
2022-11-16 22:11:09 +0100

    Dodaj pliki projektów.

---
+++ DoWhile2/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DoWhile2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int liczba;
+            Console.WriteLine("Podaj liczbę: ");
+            liczba = System.Int32.Parse(Console.ReadLine());
+            int i = 0;
+
+            do
+            {
+                i++;
+                if (liczba % i == 0)
+                    Console.WriteLine(i);
+                
+
+
+
+            } while (liczba != i);
+
+            
+            Console.ReadKey();
+
+
+
+        }
+    }
+}
